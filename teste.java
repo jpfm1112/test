37de73c2bd1b9dae0e2227fe8bf629dae53c460c
jpfm1112 <97111746+jpfm1112@users.noreply.github.com>
2022-01-04 15:44:43 +0000

    Create teste.java

---
+++ teste.java
@@ -0,0 +1,19 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class teste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner (System.in);
+		System.out.println("Escreva o primeiro número: ");
+		int numero1 = in.nextInt();
+		System.out.println("Escreva o segundo número: ");
+		int numero2 = in.nextInt();
+		int soma = numero1 + numero2;
+		System.out.println("A soma é: " + soma);
+		in.close();
+	}
+
+}
