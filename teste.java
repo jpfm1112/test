dad1810de4349059c2caf63a1aecd1d95cf037af
jpfm1112 <97111746+jpfm1112@users.noreply.github.com>
2022-01-04 16:04:07 +0000

    Update teste.java

--- teste.java
+++ teste.java
@@ -13,7 +13,9 @@ public class teste {
 		int numero2 = in.nextInt();
 		System.out.println("Escreva o terceiro número: ");
 		int numero3 = in.nextInt();
-		int soma = numero1 + numero2 + numero3;
+		System.out.println("Escreva o quarto número: ");
+		int numero4 = in.nextInt();
+		int soma = numero1 + numero2 + numero3 + numero4;
 		System.out.println("A soma é: " + soma);
 		in.close();
 	}
